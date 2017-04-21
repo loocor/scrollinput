28b58f44ebd1d752821cd12639f428d8d3263e31
Loocor <loocor@gmail.com>
2017-04-21 21:18:12 +0800

    [IMP] Exposed two parameters.

--- build.js
+++ build.js
@@ -2,55 +2,52 @@
 /* jshint strict: true */
 /* exported init */
 
-// 动态设计思路及核心参数
-//
-// @minValue: 指定输入最小值
-// @maxValue: 指定输入最大值
-// @metaValue: 指定输入精度，用于计算最低单元格
-// @majorStep: 主间隔单位，显示长线及数值
-// @minorStep: 次间隔单位，显示短线及数值
-// @scaleMode：flex, fixed 伸缩和固定
-//
-// 视觉要求（伸缩）：左右靠齐窗口两侧，随窗口伸缩实时更新尺寸，内部刻度、文字间隔对应变化；
-// 视觉效果（固定）：尺寸按照最小、最大以及精度值进行计算，窗口伸缩不影响输入框的尺寸；
-//
-// 实现途径：基于之前的参数由 jQ 生成包含数值的 div / li 区块，然后再通过 jQ 设置各自的样式；
+/*
+ 动态设计思路及核心参数
+
+ @minValue: 指定输入最小值
+ @maxValue: 指定输入最大值
+ @metaValue: 指定输入精度，用于计算最低单元格
+ @majorStep: 主间隔单位，显示长线及数值
+ @minorStep: 次间隔单位，显示短线及数值
+ @scaleMode：flex, fixed 伸缩和固定
+
+ 视觉要求（伸缩）：左右靠齐窗口两侧，随窗口伸缩实时更新尺寸，内部刻度、文字间隔对应变化；
+ 视觉效果（固定）：尺寸按照最小、最大以及精度值进行计算，窗口伸缩不影响输入框的尺寸；
+
+ 实现途径：基于之前的参数由 jQ 生成包含数值的 div / li 区块，然后再通过 jQ 设置各自的样式；
+*/
 
 $(function () {
 
-    // var scroller = $("#scrollbar");
-    // var content = $("#content");
-    // var config = {
-    //     "minValue"  : minValue ? minValue : 0,
-    //     "maxValue"  : maxValue ? maxValue : 100,
-    //     "initValue" : initValue ? initValue : 0,
-    //     "metaValue" : metaValue ? metaValue : 0.1,
-    //     "majorStep" : majorStep ? majorStep : 1,
-    //     "minorStep" : minorStep ? minorStep : 0.2,
-    //     "scaleMode" : scaleMode ? scaleMode : "fixed"
-    // };
+    // 设置初始位置及最大值
+    var initValue = 250;
+    var maxValue = 500;
 
+    // 初始化内容标尺
+    $("#content").width(300 + maxValue * 10);
     $("#content").svg({
         "onLoad" : drawIntro
     });
 
+    // 绘制标尺内容
     function drawIntro(svg) {
         var g = svg.group({
             "stroke"      : "gray",
             "strokeWidth" : 1
         });
 
-        for (var i = 0; i < 1300; i = i + 5) {
+        for (var i = 0; i < 300 + maxValue * 10; i = i + 5) {
             if (i % 25 === 0) {
                 svg.line(g, i, 40, i, 65);
             }
 
             if (i % 50 === 0) {
                 svg.line(g, i, 35, i, 65);
-                svg.text(g, i - 5, 80, ((i - 150) / 10).toString(), {
+                svg.text(g, i, 80, ((i - 150) / 10).toString(), {
                     "size"   : 6,
                     "fill"   : "#DDD",
-                    "family" : "ArialMT",
+                    "family" : "Arial",
                     "stroke" : "transparent"
                 });
             }
@@ -59,7 +56,7 @@ $(function () {
         }
     }
 
-    // 始化化滚动条
+    // 初始化滚动条
     var options = {
         "speed"                 : 0.5,
         "overscrollEffect"      : "glow",
@@ -68,15 +65,12 @@ $(function () {
 
     var scrollbar = Scrollbar.init(document.getElementById("scrollbar"), options);
 
-    scrollbar.limit.y = 0;
+    // 输出最新数值
     scrollbar.addListener(function (status) {
         $(".scrollbar-track, .overscroll-glow").remove();
         $("#size").html(status.offset.x / 10);
-
-        if (status.offset.x % 5 === 0) {
-            $("#mark").show();
-        }
     });
 
-    scrollbar.setPosition(100, 0);
+    // 设置起始位置
+    scrollbar.setPosition(initValue * 10, 0);
 });
